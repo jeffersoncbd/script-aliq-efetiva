c11c77f3fd9c6f2281e186cb872459cfc5289b95
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-07-06 15:55:24 +0000

    remove sleep e faz com que script seja encerrado somente após Enter

--- src/_domain/entities/Sleep/Entity.ts
+++
@@ -1,7 +0,0 @@
-export class Sleep {
-  for(seconds: number) {
-    return new Promise(resolve => {
-      setTimeout(resolve, seconds * 1000)
-    })
-  }
-}
--- src/application.ts
+++ src/application.ts
@@ -1,11 +1,9 @@
 import attachments from '../anexos.config.ts'
 import { AskTheUserForInformation } from './_domain/entities/AskTheUserForInformation/Entity.ts'
-import { Sleep } from './_domain/entities/Sleep/Entity.ts'
 import { AskAttachmentUseCase } from './_domain/useCases/AskAttachment/UseCase.ts'
 
 const asker = new AskTheUserForInformation()
 const attachmentAsker = new AskAttachmentUseCase(asker)
-const sleep = new Sleep()
 
 
 const RBT12 = Number(await asker.ask('RBT dos ultimos 12 meses'))
@@ -17,11 +15,11 @@ if (selectedAttachment !== null) {
   selectedAttachment.forEach(billingRange => {
     if (RBT12 > billingRange.RBT.min && RBT12 < billingRange.RBT.max) {
       const effectiveRate = ((RBT12 * billingRange.aliq) - billingRange.parcDed ) / RBT12
-      console.log(`${effectiveRate * 100} %`)
+      console.log(`\nAliquota efetiva: ${effectiveRate * 100} %`)
     }
   })
 } else {
   console.log('Anexo não cadastrado')
 }
 
-await sleep.for(120)
+await asker.ask('\nPrecione Enter para sair...')
